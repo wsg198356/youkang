ac253500d9f8da9d7613e664845370d2c67eebfe
wsg198356 <1064860088@qq.com>
2018-01-22 10:34:19 +0800

    asdf

--- application/admin/controller/Log.php
+++ application/admin/controller/Log.php
@@ -21,6 +21,6 @@ class Log extends Base
         $count = Db::name('log')->where($map)->count();
         $allpage = intval(ceil($count / $limits));
         $lists = Db::name('log')->where($map)->page($Nowpage, $limits)->order('add_time desc')->select();
-        $Ip = new IpLocation();
+        $Ip = new IpLocation("UTFWry.dat");
     }
 }
